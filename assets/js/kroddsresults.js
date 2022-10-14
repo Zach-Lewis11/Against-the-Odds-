2cb143cb3e1e348dc0a11bc6a158610730dfa820
mtbell412 <mtbell412@gmail.com>
2022-10-14 09:39:22 -0500

    local storage

--- assets/js/kroddsresults.js
+++ assets/js/kroddsresults.js
@@ -24,6 +24,8 @@ function getOdds(league,team) {
     // console.log(sport.value);
     // var league ='americanfootball_nfl';
     //var league = sport.value;
+    localStorage.setItem("League",JSON.stringify(league));
+    localStorage.setItem("Team",JSON.stringify(team));
 
     console.log('function starts!')
 
@@ -44,60 +46,8 @@ function getOdds(league,team) {
         // console.log(typeof team !== 'undefined');
 
         //if team input is defined
-
-        if (typeof team !== 'undefined'){
-            for (var i = 0; i < data.length; i++) {
-                // Team is equal to the home or away team
-                //console.log(team == (data[i].home_team || data[i].away_team));
-                // Team is equal to the home or away team
-                // console.log(team);
-                // console.log(data[i].home_team);
-                // console.log(data[i].away_team);
-
-                // console.log(team === data[i].home_team || team === data[i].away_team);
-                if (team === data[i].home_team || team === data[i].away_team){
-                    //assigning the 
-                    var h2h = data[i].bookmakers[0].markets[0];
-                    var spreads = data[i].bookmakers[0].markets[1];
-                    var totals = data[i].bookmakers[0].markets[2];
-                    var gameTime = data[i].commence_time;
-                    // console.log(gameTime);
-                    // console.log(h2h);
-                    // console.log(spreads);
-                    // console.log(totals);
-                    
-                    //
-                    // teamTitle.textContent = team + ' Game  Odds';
-
-                    time.textContent = gameTime;
-                    // oddsData.appendChild(time);
-
-                    var geth2h = Object.keys(h2h.outcomes[0]);
-                    mLine.innerHTML = h2h.key + ': ' + h2h.outcomes[0].name + '-' + geth2h[1] + h2h.outcomes[0].price + 
-                    '<br>' +  h2h.outcomes[1].name + geth2h[1] + '-' + h2h.outcomes [1].price + '</br>';
-                    // oddsData.appendChild(mLine);
-                    
-                    var getPoints = Object.keys(spreads.outcomes[0]);
-                    points.innerHTML = spreads.key + ': ' + '<br>' + spreads.outcomes[0].name + '<br>' + getPoints[2] + spreads.outcomes[0].point + '<br>' +  spreads.outcomes[1].name + getPoints[2] + '-' + spreads.outcomes[1].point + '</br>';
-                    // oddsData.appendChild(points);
-
-                    var getTotals = Object.keys(totals.outcomes[0]);
-                    overUnder.innerHTML = totals.key + ': ' + totals.outcomes[0].name + '<br>' + getTotals[2] + totals.outcomes[0].point + getTotals[1]  + totals.outcomes[1].price + '</br>';
-                    
-                    // oddsData.appendChild(overUnder);  
-
-                    //consolelogs to prove its working!
-                    console.log(h2h.key + ': ' + h2h.outcomes[0].name + '-' + geth2h[1] + h2h.outcomes[0].price + 
-                    '<br>' +  h2h.outcomes[1].name + geth2h[1] + '-' + h2h.outcomes [1].price + '</br>');
-
-                    console.log(spreads.key + ': ' + '<br>' + spreads.outcomes[0].name + '<br>' + getPoints[2] + spreads.outcomes[0].point + '<br>' +  spreads.outcomes[1].name + getPoints[2] + '-' + spreads.outcomes[1].point + '</br>')
-
-                    console.log(totals.key + ': ' + totals.outcomes[0].name + '<br>' + getTotals[2] + totals.outcomes[0].point + getTotals[1]  + totals.outcomes[1].price + '</br>');
-                }
-            }
-        }else {
-            return('No Games');
-        }
+        localStorage.setItem("TeamData",JSON.stringify(data));
 
     });
 }
+
